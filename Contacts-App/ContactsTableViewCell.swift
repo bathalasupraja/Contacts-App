d0532ac33654854a8290f1ff25cf2e1bdd0d3a1d
bathalasupraja <bathalasupraja2@gmail.com>
2024-04-29 21:42:30 +0530

    Added inputView  and textField validations.
    * Added datePicker and its functionalities.
    * Added keypad type functionality and  confirm textField delegate.

--- Contacts-App/ContactsTableViewCell.swift
+++ Contacts-App/ContactsTableViewCell.swift
@@ -20,14 +20,56 @@ class ContactsTableViewCell: UITableViewCell {
     
     weak var delegate: ContactsTableViewCellDelegate?
     
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        nameTextField.delegate = self
+        nameTextField.resignFirstResponder()
+    }
+    
     func prepareWithContacts(_ contactsField: ContactsDataModel, delegate: ContactsTableViewCellDelegate?, indexPath: IndexPath) {
-       // nameTextField.keyboardType = getKeypadTypeForIndexPath(indexPath)
+        nameTextField.inputView = getInputViewAtIndexPath(indexPath)
+        nameTextField.keyboardType = getKeypadTypeForIndexPath(indexPath)
         nameLabel.text = contactsField.name
         nameTextField.placeholder = contactsField.placeholder
         nameTextField.text = contactsField.value
         nameTextField.resignFirstResponder()
         self.delegate = delegate
         nameTextField.tag = indexPath.row
+    }
+    
+    func getInputViewAtIndexPath(_ indexPath: IndexPath) -> UIView? {
+        switch indexPath.row {
+        case 5:
+            let datePicker = UIDatePicker()
+            datePicker.preferredDatePickerStyle = .wheels
+            datePicker.addTarget(self, action: #selector(didChangeDate), for: .valueChanged)
+            datePicker.datePickerMode = .date
+            return datePicker
+        default:
+            return nil
+        }
+    }
+    
+    @objc func didChangeDate(_ sender: UIDatePicker) {
+        print(sender.date)
+        let formatter = DateFormatter()
+        formatter.dateFormat = "dd/MM/yyyy"
+        nameTextField.text = formatter.string(from: sender.date)
+    }
+    
+    func getKeypadTypeForIndexPath(_ indexPath: IndexPath) -> UIKeyboardType {
+        switch indexPath.row {
+        case 0:
+            return .default
+        case 1,2:
+            return .namePhonePad
+        case 3:
+            return .phonePad
+        case 4:
+            return .emailAddress
+        default:
+            return .default
+        }
     }
 }
 
